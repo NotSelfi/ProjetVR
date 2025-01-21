a32e60f7310751d268342d890b522ef9272e2271
lesly <lesly97420@gmail.com>
2025-01-21 15:14:47 +0100

    add Kyle

--- Assets/Script/Photon/GameManager.cs
+++ Assets/Script/Photon/GameManager.cs
@@ -30,16 +30,16 @@ namespace Com.MyCompany.MyGame
         void Start()
         {
             Instance = this;
-            if (playerPrefab == null)
+           /* if (playerPrefab == null)
             {
                 Debug.LogError("<Color=Red><a>Missing</a></Color> playerPrefab Reference. Please set it up in GameObject 'Game Manager'", this);
             }
-            else
+           else
             {
                 Debug.LogFormat("We are Instantiating LocalPlayer from {0}", SceneManager.GetActiveScene().name);
                 // we're in a room. spawn a character for the local player. it gets synced by using PhotonNetwork.Instantiate
                 PhotonNetwork.Instantiate(this.playerPrefab.name, new Vector3(0f, 5f, 0f), Quaternion.identity, 0);
-            }
+            }*/
 
             if (PlayerManager.LocalPlayerInstance == null)
             {
