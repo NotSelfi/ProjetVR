540f3a605832891134120b5b60e40d10c31f1eb8
Meithal <hurin8888@gmail.com>
2025-02-01 05:18:10 +0100

    add fusion sdk and fusion scene

---
+++ Assets/ivo/IvoPlayerSpawner.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fusion;
+
+
+public class IvoPlayerSpawner : SimulationBehaviour, IPlayerJoined
+{
+    public GameObject PlayerPrefab;
+
+    public void PlayerJoined(PlayerRef player)
+    {
+        if (player == Runner.LocalPlayer)
+        {
+            Runner.Spawn(PlayerPrefab, new Vector3(0, 1, 0), Quaternion.identity);
+        }
+    }
+}
+
