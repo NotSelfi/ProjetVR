aeec498a33c05752ad32b3bd5393cfad65d13942
Meithal <hurin8888@gmail.com>
2025-02-07 17:00:09 +0100

    working state

--- Assets/ivo/IvoPlayerSpawner.cs
+++ Assets/ivo/IvoPlayerSpawner.cs
@@ -1,18 +1,19 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using Fusion;
 
 
-public class IvoPlayerSpawner : SimulationBehaviour, IPlayerJoined, INetworkObjectProvider
+public class IvoPlayerSpawner : SimulationBehaviour, IPlayerJoined
 {
-    public IvoIGetSpawnableGO PlayerPrefab;
+    //public IvoIGetSpawnableGO PlayerPrefab;
+    public GameObject PlayerPrefab;
+
 
     public void PlayerJoined(PlayerRef player)
     {
         if (player == Runner.LocalPlayer)
         {
-            Runner.Spawn(PlayerPrefab.IvoGetSpawnablerGameObject(), new Vector3(0, 1, 0), Quaternion.identity);
+            //Runner.Spawn(PlayerPrefab.IvoGetSpawnablerGameObject(), new Vector3(0, 1, 0), Quaternion.identity);
+            Runner.Spawn(PlayerPrefab, new Vector3(0, 1, 0), Quaternion.identity);
         }
     }
 }
